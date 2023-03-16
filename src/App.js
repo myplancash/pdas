48ff817e9ca2fce2d72e11a5d268667a3f5d8a07
Sergio Esteban Torres <myplancash@gmail.com>
2023-03-16 12:27:39 -0500

    added a description text for each card in the app

--- src/App.js
+++ src/App.js
@@ -8,18 +8,42 @@ import 'bulma/css/bulma.css'
 const App = () => {
   return (
     <div>
-      <div>Cards info</div>
+      <section className="hero">
+        <div className="hero-body">
+          <p className="title">
+            Primary Cards Info
+          </p>
+          <p className="subtitle">
+            Let's check our cards
+          </p>
+        </div>
+      </section>
       <div className="container">
         <div className="section">
           <div className="columns">
             <div className="column is-4">
-              <ProfileCard title='This is the first Title ' handle='@sergio23' image={Alexa}/>
+              <ProfileCard 
+                title='This is the first Title' 
+                handle='@sergio23' 
+                image={Alexa}
+                description="Ipsum has been the industry's standard dummy text ever since the 1500's, when an unknown printer took a galley of type and scrambled it to make a type."
+              />
             </div>
             <div className="column is-4">
-              <ProfileCard title='This is the Second Title' handle='@juanC4' image={Cortana}/>
+              <ProfileCard 
+                title='This is the Second Title' 
+                handle='@juanC4' 
+                image={Cortana}
+                description="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+              />
             </div>
             <div className="column is-4">
-              <ProfileCard title='This is the Third Title ' handle='@maricel12' image={Siri}/>
+              <ProfileCard 
+                title='This is the Third Title' 
+                handle='@maricel12' 
+                image={Siri}
+                description="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."
+              />
             </div>
           </div>
         </div>
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const ProfileCard = ({title, handle, image}) => {
+
+const ProfileCard = ({title, handle, image, description}) => {
   return (
     <div className="card">
       <div className="card-image">
@@ -8,17 +9,14 @@ const ProfileCard = ({title, handle, image}) => {
           <img src={image} alt="title of the card info" />
         </figure>
       </div>
+      
       <div className="card-content">
         <div className="media-content">
           <h1 className="title is-4">{title}</h1>
           <p className="subtitle is-6">{handle}</p>
         </div>
         <div className="content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Phasellus nec iaculis mauris.
-          <span>@bulmspanio</span>.
-          <span>#css</span> 
-          <span>#responsive</span>
+          {description}
           <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
         </div>
       </div>
